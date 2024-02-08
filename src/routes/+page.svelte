46352823b444af51281f68248ec0c1e815218b69
JinHyeok <gjwlsgur123@gmail.com>
2024-02-08 17:46:46 +0900

    TailwindCss 설정 & Path Alias 설정

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+	import Test from '$components/Test.svelte';
+</script>
+
+<h1 class="font-bold text-4xl">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Test />
