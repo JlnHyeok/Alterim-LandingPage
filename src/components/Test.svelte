2b1d5974ba176ec74b6710bb64d18d653cb60755
JinHyeok <gjwlsgur123@gmail.com>
2024-02-08 18:26:00 +0900

    Color Palatte & Warning 수정 & 폴더 구조 작업

--- src/components/Test.svelte
+++ src/components/Test.svelte
@@ -1,1 +1,3 @@
-<div>Hello Test!!</div>
+<div class="bg-black font-['Pretendard'] text-font-highlight/75">
+	Meet Your Other Self in the Web3
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+delete colors['lightBlue'];
+delete colors['warmGray'];
+delete colors['trueGray'];
+delete colors['coolGray'];
+delete colors['blueGray'];
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				...colors,
+				font: {
+					light: '#f5f5f5',
+					dark: '#fff',
+					highlight: '#AEF6CB',
+					sub: '#F2F2F2',
+					subHighlight: '#C4DACC'
+				}
+			}
+		}
 	},
 	plugins: []
 };
