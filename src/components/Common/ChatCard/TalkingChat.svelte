dcf8032ca990c89f02de8d0c80bedb09787e310b
JinHyeok <gjwlsgur123@gmail.com>
2024-02-08 19:00:11 +0900

    Feat: Chatting Card Component 진행중

---
+++ src/components/Common/ChatCard/TalkingChat.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let message: string;
+</script>
+
+<span
+	class="absolute bg-[rgba(248,255,246,0.10)] rounded-[40px] p-3 flex flex-row gap-2.5 items-end justify-start shrink-0 w-80"
+	style="box-shadow: 0px 3.09px 12.36px 0px rgba(0, 0, 0, 0.08); ">{message}</span
+>
--- src/components/Test.svelte
+++ src/components/Test.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+	import TalkingChat from './Common/ChatCard/TalkingChat.svelte';
+</script>
+
 <div class="bg-black font-['Pretendard'] text-font-highlight/75">
 	Meet Your Other Self in the Web3
+</div>
+
+<div class="mt-3 h-20">
+	<TalkingChat message="Good Morinig Guyszzaasaasaㄴqsasq sq  s q s!" />
 </div>
