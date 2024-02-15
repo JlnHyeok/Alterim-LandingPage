6d9bb8d3b5f6ba5e7916e1e7935a0b707da16e80
JinHyeok <gjwlsgur123@gmail.com>
2024-02-16 02:51:32 +0900

    Design: Top 부분 Animation 추가

--- src/components/Intro/Intro.svelte
+++ src/components/Intro/Intro.svelte
@@ -1,25 +1,51 @@
 <script lang="ts">
+	import { scale } from 'svelte/transition';
+	import { inview } from 'svelte-inview';
 	import { Logo, ScrollDown } from '$lib/assets/images';
 	import AvatarList from './AvatarList.svelte';
+
+	export let isInView: boolean;
 </script>
 
-<div class="relative w-full h-screen text-white">
+<div
+	class="relative w-full h-screen text-white"
+	use:inview={{ unobserveOnEnter: false, rootMargin: '-40%' }}
+	on:inview_change={({ detail }) => {
+		const { inView } = detail;
+		isInView = inView;
+	}}
+>
 	<!-- TITLE SECTION -->
 	<div class="w-full h-1/2 flex flex-col items-center relative">
 		<!-- LOGO -->
-		<div class="w-16 h-20 mt-[160px]">
-			<img src={Logo} alt="logo" class="object-cover" />
-		</div>
+		{#if isInView}
+			<div
+				class="w-16 h-20 mt-[160px]"
+				in:scale={{ duration: 400, delay: 300, start: 1.5, opacity: 0 }}
+			>
+				<img src={Logo} alt="logo" class="object-cover" />
+			</div>
+		{/if}
 
 		<!-- MAIN TEXT -->
-		<div class="font-Pretendard_Regular font-normal text-white text-[48px]">
-			<h1>Meet Your Other Self in the Web3</h1>
-		</div>
+		{#if isInView}
+			<div
+				class="font-Pretendard_Regular font-normal text-white text-[48px]"
+				in:scale={{ duration: 400, delay: 500, start: 1.5, opacity: 0 }}
+			>
+				<h1>Meet Your Other Self in the Web3</h1>
+			</div>
+		{/if}
 
 		<!-- SUB TEXT -->
-		<div class="font-Pretendard_Light text-[24px] text-[#ffffffbf]">
-			<p>Where Your Digital Clone Comes to Life powered by AI</p>
-		</div>
+		{#if isInView}
+			<div
+				class="font-Pretendard_Light text-[24px] text-[#ffffffbf]"
+				in:scale={{ duration: 800, delay: 800, start: 1, opacity: 0 }}
+			>
+				<p>Where Your Digital Clone Comes to Life powered by AI</p>
+			</div>
+		{/if}
 
 		<a href="#welcome" class="absolute flex flex-col items-center h-14 -bottom-24 z-20">
 			<span
@@ -34,6 +60,6 @@
 
 	<!-- AVATAR SECTION -->
 	<div class="w-full h-[calc(50%-80px)] relative text-center flex items-end">
-		<AvatarList />
+		<AvatarList bind:isInView />
 	</div>
 </div>
