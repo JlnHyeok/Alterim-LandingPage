041548630b8758ff8897d5ace1d72e7fcaf44a2f
JinHyeok <gjwlsgur123@gmail.com>
2024-02-19 22:43:04 +0900

    Fix: Header Opacity 수정

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -2,7 +2,9 @@
 	import Title from '$components/Common/Title.svelte';
 </script>
 
-<div class="h-20 w-screen z-20 fixed bg-opacity-0 flex justify-between items-center px-2 md:px-5">
+<div
+	class="fixed z-20 flex h-20 w-screen items-center justify-between bg-black bg-opacity-100 px-2 md:px-5"
+>
 	<div>
 		<Title iconColor="white" textColor="white" />
 	</div>
@@ -10,15 +12,15 @@
 	<a
 		href="https://alterim-ai.gitbook.io/welcometoalterim/"
 		target="_blank"
-		class="bg-[#ffffff] rounded-[40px] py-2 flex flex-row gap-2.5 items-center justify-center w-[100px] md:w-[200px] relative overflow-hidden"
+		class="relative flex w-[100px] flex-row items-center justify-center gap-2.5 overflow-hidden rounded-[40px] bg-[#ffffff] py-2 md:w-[200px]"
 		style="box-shadow: 0px 9px 34px 0px rgba(212, 212, 212, 0.50); "
 	>
 		<div
-			class="text-[#020202] text-center font-Pretendard_Regular text-sm md:text-lg font-normal relative flex items-center justify-center gap-2"
+			class="relative flex items-center justify-center gap-2 text-center font-Pretendard_Regular text-sm font-normal text-[#020202] md:text-lg"
 		>
 			<span>Document</span>
 			<svg
-				class="shrink-0 w-[9.59px] h-[9.6px] relative overflow-visible hidden md:block"
+				class="relative hidden h-[9.6px] w-[9.59px] shrink-0 overflow-visible md:block"
 				style=""
 				width="10"
 				height="10"
